694790bc8070e8d516f1a001e522d7429c15f882
Laz <lazbratton@gmail.com>
2025-07-23 22:52:09 +1200

    Exercises to 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(valToConvert) {
+  let tempInCelsius = ((valToConvert - 32) * 0.55555555555).toFixed(1);
+  return Number.parseFloat(tempInCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(valToConvert) {
+  let tempInFahrenheit = ((valToConvert * 1.8) + 32).toFixed(1);
+  return Number.parseFloat(tempInFahrenheit);
 };
 
 // Do not edit below this line
