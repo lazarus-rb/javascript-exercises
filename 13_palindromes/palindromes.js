4bd234921f6ce91bedb84a7d7fbcc83fdcc26f7b
Laz <114888437+lazarus-rb@users.noreply.github.com>
2025-07-30 09:21:05 +1200

    palindrome completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let letterOnlyString = inputString.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+        return letterOnlyString == letterOnlyString.split('').reverse().join('');
 };
 
 // Do not edit below this line
