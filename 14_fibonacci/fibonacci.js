2a0af362d6410d9150b69a8c3def2596adb4183d
Laz <114888437+lazarus-rb@users.noreply.github.com>
2025-07-30 11:22:07 +1200

    fibonacci sequence complete

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    
+
+    if(num <= 1) {
+        if(num<0)
+            return 'OOPS';
+        if(num == 0) 
+            return 0;
+        else
+            return 1;
+    }
+
+    num += 1; 
+
+    let sequence = [0, 1, 1];
+
+
+    for(let i = 3; i < num; i++) {
+        let n1 = sequence[i - 1];
+        let n2 = sequence[i - 2];
+
+        let n = n1 + n2;
+
+        sequence.push(n);
+
+    }
+    
+    return sequence[num-1];
 
 };
 
