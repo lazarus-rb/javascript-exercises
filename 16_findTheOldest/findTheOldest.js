0fc612b1911c48d9bd6b714e39dc8f8a4aeb6df5
Laz <114888437+lazarus-rb@users.noreply.github.com>
2025-07-30 12:18:08 +1200

    completed exercises

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    let oldestPerson;
+    let age = 0;
+    const currentDate = new Date();
+    let oldestYear = currentDate.getFullYear();
 
+    peopleArray.forEach((people) => {
+
+        if(people.yearOfDeath !== undefined) {
+            oldestYear = people.yearOfDeath;            
+        }
+        
+        let newAge = oldestYear - people.yearOfBirth;
+
+        if(newAge>age) {
+            age = newAge;
+            oldestPerson = people;
+        }
+
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
